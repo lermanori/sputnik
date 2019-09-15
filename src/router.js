0e94daf0694bdda868b8f1f77860933e6bfa24f2
lermanori <lermanori@gmail.com>
2019-09-15 22:27:20 +0300

    gh3

--- src/router.js
+++ src/router.js
@@ -6,17 +6,8 @@ Vue.use(Router)
 
 export default new Router({
   routes: [{
-      path: '/',
-      name: 'home',
-      component: Home
-    },
-    {
-      path: '/sputnik',
-      name: 'sputnik',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import( /* webpackChunkName: "about" */ './views/test.vue')
-    }
-  ]
+    path: '/',
+    name: 'home',
+    component: Home
+  }, ]
 })
